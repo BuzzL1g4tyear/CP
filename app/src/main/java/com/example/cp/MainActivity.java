38a53a024d4220ce3c578c257fd70ae6609bede0
BuzzL1g4tyear <70737193+BuzzL1g4tyear@users.noreply.github.com>
2020-12-20 15:55:00 +0300

    retrofit

--- app/src/main/java/com/example/cp/MainActivity.java
+++ app/src/main/java/com/example/cp/MainActivity.java
@@ -1,32 +1,122 @@
 package com.example.cp;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText textNum, textName;
+    ListView List;
+    ListAdapter ListAdapter;
+
+    public static String server = "db.cp.by";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List = findViewById(R.id.listView);
     }
-    public void onClickAdd(View view){
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, "", new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
 
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
+    public void onClickAdd(View view) {
 
+    }
+
+    class SendData extends AsyncTask<Void, Void, Void> {
+
+        String resultString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+
+                String myURL = "https://" + server + "/server.php";
+                byte[] data = null;
+                InputStream is = null;
+
+                String parameters = "" ;
+                try {
+                    URL url = new URL(myURL);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setReadTimeout(10000);
+                    conn.setConnectTimeout(15000);
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Connection", "Keep-Alive");
+                    conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                    conn.setRequestProperty("Content-Length", "" + Integer.toString(parameters.getBytes().length));
+                    conn.setDoOutput(true);
+                    conn.setDoInput(true);
+                    // конвертируем передаваемую строку в UTF-8
+                    data = parameters.getBytes("UTF-8");
+                    OutputStream os = conn.getOutputStream();
+
+                    // передаем данные на сервер
+                    os.write(data);
+                    os.flush();
+                    os.close();
+                    data = null;
+                    conn.connect();
+                    int responseCode = conn.getResponseCode();
+                    // передаем ответ сервер
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+                    if (responseCode == 200) {    // Если все ОК (ответ 200)
+                        is = conn.getInputStream();
+
+                        byte[] buffer = new byte[8192]; // размер буфера
+
+                        // Далее так читаем ответ
+                        int bytesRead;
+                        while ((bytesRead = is.read(buffer)) != -1) {
+                            baos.write(buffer, 0, bytesRead);
+                        }
+                        data = baos.toByteArray();
+                        resultString = new String(data, "UTF-8");  // сохраняем в переменную ответ сервера, у нас "OK"
+                    } else {
+                    }
+                    conn.disconnect();
+
+                } catch (MalformedURLException e) {
+
+                    //resultString = "MalformedURLException:" + e.getMessage();
+                } catch (IOException e) {
+
+                    //resultString = "IOException:" + e.getMessage();
+                } catch (Exception e) {
+
+                    //resultString = "Exception:" + e.getMessage();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        })
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            @SuppressLint("ShowToast") Toast toast = Toast.makeText(getApplicationContext(), "Данные переданы!", Toast.LENGTH_SHORT);
+        }
+
     }
 }
