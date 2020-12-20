44298dcb4eb8f0510ca3bd4581426d5afb6a56c4
BuzzL1g4tyear <70737193+BuzzL1g4tyear@users.noreply.github.com>
2020-12-20 16:04:51 +0300

    retrofit1

--- app/src/main/java/com/example/cp/MainActivity.java
+++ app/src/main/java/com/example/cp/MainActivity.java
@@ -1,22 +1,12 @@
 package com.example.cp;
 
-import android.annotation.SuppressLint;
-import android.os.AsyncTask;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
