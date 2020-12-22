07e7f41dc4989a6d88631e5ada0aac4b47ddd53c
BuzzL1g4tyear <70737193+BuzzL1g4tyear@users.noreply.github.com>
2020-12-22 18:54:08 +0300

    CP

---
+++ app/src/main/java/com/example/cp/ConnectionHelper.java
@@ -0,0 +1,39 @@
+package com.example.cp;
+
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.*;
+
+public class ConnectionHelper {
+
+    public static final String SERVER = "db.cp.by";
+    public static final String DBNAME = "Andriod";
+    public static final String LOGIN = "androidtest";
+    public static final String PAS = "LB@m$V#pJCj6fA";
+    public static final int PORT = 7878;
+
+    private int id;
+    private String num;
+    private String name;
+
+    public Connection getCon() {
+
+        Connection con = null;
+        String ConnURL = "jdbc:jtds:sqlserver://" + SERVER + ":" + PORT + ":/" + DBNAME+";";
+
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            con = DriverManager.getConnection(ConnURL, LOGIN, PAS);
+            Log.d("MyTag", "Подключилось");
+        } catch (SQLException | ClassNotFoundException e) {
+            Log.d("MyTag", "Увы");
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+
+}
--- app/src/main/java/com/example/cp/MainActivity.java
+++ app/src/main/java/com/example/cp/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.cp;
 
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,16 +25,57 @@ public class MainActivity extends AppCompatActivity {
     ListView List;
     ListAdapter ListAdapter;
 
-    public static String server = "db.cp.by";
+    ConnectionHelper connect = new ConnectionHelper();
+    Connection connection = connect.getCon();
+    Statement st = null;
+    ResultSet rs = null;
+
+    public static final String SHOWALL = "SELECT * FROM CATALOG";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textNum = findViewById(R.id.txtNum);
+        textName = findViewById(R.id.txtName);
         List = findViewById(R.id.listView);
     }
+    public final class AsyncRequest extends AsyncTask<String, Void, JSONArray> {
+        protected JSONArray doInBackground(String... query) {
 
+            JSONArray resultSet = new JSONArray();
+            try {
+                st = connection.createStatement();
+                rs = st.executeQuery(SHOWALL);
+                int columnCount = rs.getMetaData().getColumnCount();
+                if (rs != null) {
+                    JSONObject rowObject = new JSONObject();
+                    for (int i = 1; i <= columnCount; i++) {
+                        rowObject.put(rs.getMetaData().getColumnName(i), (rs.getString(i) != null) ? rs.getString(i) : "");
+                    }
+                    resultSet.put(rowObject);
+                }
+            } catch (SQLException | JSONException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (rs != null) rs.close();
+                    if (st != null) st.close();
+                    if (connection != null) connection.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e.getMessage());
+                }
+            }
+
+            return resultSet;
+        }
+    }
     public void onClickAdd(View view) {
+
 
     }
 
+    public void Toast(String mess) {
+        Toast.makeText(this, mess, Toast.LENGTH_LONG).show();
+    }
 }
