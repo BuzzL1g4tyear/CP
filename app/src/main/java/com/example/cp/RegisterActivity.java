517a29163e7b3b27fe99b25238ba9ecdeba7fe47
BuzzL1g4tyear <70737193+BuzzL1g4tyear@users.noreply.github.com>
2021-02-08 15:52:21 +0300

    3

--- app/src/main/java/com/example/cp/RegisterActivity.java
+++ app/src/main/java/com/example/cp/RegisterActivity.java
@@ -2,12 +2,34 @@ package com.example.cp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+    TextView u_nameText, u_famText, phoneText, loginText, EmailText, PasText;
+    String u_name, u_fam, phone, login, Email, Pas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+    }
+    public void onClickAdd(View view){
+        u_nameText = (TextView) findViewById(R.id.U_nameTXT);
+        u_famText = (TextView) findViewById(R.id.U_famTXT);
+        phoneText = (TextView) findViewById(R.id.phoneTXT);
+        loginText = (TextView) findViewById(R.id.LoginTXT);
+        PasText = (TextView) findViewById(R.id.PasTXT);
+        EmailText = (TextView) findViewById(R.id.EmailTXT);
+
+        u_name = u_nameText.getText().toString();
+        u_fam = u_famText.getText().toString();
+        phone = phoneText.getText().toString();
+        login = loginText.getText().toString();
+        Pas = PasText.getText().toString();
+        Email = EmailText.getText().toString();
+
+
     }
 }
