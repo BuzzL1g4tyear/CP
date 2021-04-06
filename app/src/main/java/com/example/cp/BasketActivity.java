36a1796b3486386e45e40343a7f5e1506ea4cd87
BuzzL1g4tyear <70737193+BuzzL1g4tyear@users.noreply.github.com>
2021-04-06 17:44:57 +0300

    upd 06.04(basketToolbar)

--- app/src/main/java/com/example/cp/BasketActivity.java
+++ app/src/main/java/com/example/cp/BasketActivity.java
@@ -3,14 +3,77 @@ package com.example.cp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BasketActivity extends AppCompatActivity {
 
+    private BaskedAdapter baskedAdapter;
+
     public static final String SHOWBASKETQ = "";
+    ConnectionHelper connect = new ConnectionHelper();
+    Connection connection = connect.getCon();
+    Statement st = null;
+    ResultSet rs = null;
+    ListView listView;
+
+    private List<ListItem> arrayListItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basket);
+
+        arrayListItem = new ArrayList<>();
+        listView = findViewById(R.id.basketListView);
+
+        baskedAdapter = new BaskedAdapter(this, R.layout.item_list, arrayListItem, getLayoutInflater());
     }
+
+    Runnable showItems = new Runnable() {
+        ListItem listItem;
+
+        @Override
+        public void run() {
+            try {
+                st = connection.createStatement();
+                rs = st.executeQuery(SHOWBASKETQ);
+
+                if (rs != null) {
+                    int columnCount = rs.getMetaData().getColumnCount();
+                    while (rs.next()) {
+                        ListItem items = new ListItem();
+
+                        for (int i = columnCount; i <= columnCount; i++) {
+                            items.setGroup(rs.getString(1).trim());
+                            items.setBrand(rs.getString(2).trim());
+                            items.setCatNum(rs.getString(3).trim());
+                            items.setName(rs.getString(4).trim());
+                            items.setPrice(rs.getFloat(5));
+                            items.setAvailable(rs.getString(6).trim());
+                            arrayListItem.add(items);
+                            baskedAdapter.notifyDataSetChanged();
+                        }
+                        listItem = items;
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (rs != null) rs.close();
+                    if (st != null) st.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e.getMessage());
+                }
+            }
+        }
+    };
+
 }
